b155b9f662b57db29a9c719fb3d899c9b9395277
pythcoiner <pythcoiner@proton.me>
2026-08-16 15:58:06 +0000

    bwk-qr: test generation and scanning

--- qr/tests/plain.rs
+++ qr/tests/plain.rs
@@ -1,25 +1,14 @@
 #![cfg(all(feature = "gen", feature = "scan"))]
 
-use bwk_qr::{
-    config::Config,
-    decoder::{Decoded, Decoder},
-    encoder::Encoder,
-};
+mod common;
+
+use bwk_qr::{config::Config, decoder::Decoder};
+
+use common::{assert_round_trip, ADDRESS};
 
 #[test]
 fn plain_text_round_trips_through_rendered_qr() {
-    let encoder = Encoder::new(Config::default()).unwrap();
-    let mut decoder = Decoder::new(Config::default()).unwrap();
-    let image = encoder
-        .encode_text("bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh")
-        .unwrap();
-    let decoded = decoder.process(&image).unwrap();
-    assert_eq!(
-        decoded,
-        vec![Decoded::Text(
-            "bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh".to_string()
-        )]
-    );
+    assert_round_trip(ADDRESS);
 }
 
 #[test]
